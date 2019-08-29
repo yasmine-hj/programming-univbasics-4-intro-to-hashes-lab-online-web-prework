42e8c6b570b464fd6306b19c2d0fec7d9a9ad1b9
Yasmine Hajaji <yasminehajaji95@gmail.com>
2019-08-29 15:47:00 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -19,7 +19,7 @@ def my_hash_creator(key, value)
 end
 
 def read_from_hash(hash, key)
-  # return the correct value using the hash and key parameters
+  {# return the correct value using the hash and key parameters}
 end
 
 def update_counting_hash(hash, key)
